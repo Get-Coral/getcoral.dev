cf4853989aa29d319627c7eb448f497d2d093c1d
Elian Van Cutsem <hello@elian.codes>
2026-04-12 20:24:01 +0200

    chore: update site

--- astro.config.ts
+++ astro.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-	site: "https://kit.elian.codes",
+	site: "https://getcoral.dev",
 	integrations: [solidJs(), sitemap()],
 	vite: {
 		plugins: [tailwindcss()],
